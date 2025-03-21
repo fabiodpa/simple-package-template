4d6f27308a8345db8944e70acaa34de5fabcd5f7
fabiodpa <fabio.dp.alves@gmail.com>
2025-03-21 17:57:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Teste de Criação de Pacotes",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/fabiodpa/simple-package-template"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
